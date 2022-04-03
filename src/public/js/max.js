a739b6cdd667f843c956b16247afbd6135e9cf7d
MarceMatos <101466345+MarceMatos@users.noreply.github.com>
2022-04-03 16:40:45 -0500

    Update max.js

--- src/public/js/max.js
+++ src/public/js/max.js
@@ -1,4 +1,19 @@
+//Initialice the map
 var map = L.map('map-template');
 TileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 L.tileLayer(TileURL).addTo(map);
 PolyArray = [];
+
+Lat = 10.994326;
+Lon = -74.805578;
+map.setView([Lat,Lon],14);
+
+//Create icon for marker
+var MapIcon = L.Icon.extend({
+    options: {
+        iconSize: [40,40],
+        iconAnchor: [22,94],
+        popupAnchor: [-3,-76]
+    }
+})
+
