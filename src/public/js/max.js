e7a63ad43a70015f3bf815e07acfe94bc5bc68f2
MarceMatos <101466345+MarceMatos@users.noreply.github.com>
2022-04-03 17:34:14 -0500

    Update max.js

--- src/public/js/max.js
+++ src/public/js/max.js
@@ -1,1 +1,9 @@
+//Initialice the map
+var map = L.map('map-template');
+TileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+L.tileLayer(TileURL).addTo(map);
+PolyArray = [];
 
+Lat = 10.994326;
+Lon = -74.805578;
+map.setView([Lat,Lon],14);
