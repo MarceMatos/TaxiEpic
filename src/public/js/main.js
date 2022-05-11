025173652c00dd9b31ab5649018a721617e47435
Gero Ayala <101621679+SaintGero@users.noreply.github.com>
2022-05-10 22:59:17 -0500

    del shad

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -8,11 +8,11 @@ PolyArrayT2 = [];
 
 var greenIcon = new L.Icon({
   iconUrl: 'https://github.com/pointhi/leaflet-color-markers/blob/master/img/marker-icon-red.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
-  shadowSize: [41, 41]
+  
 });
 
 
